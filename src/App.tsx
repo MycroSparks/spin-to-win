0c6d3183b7292aec412373440a31b5e457a0c9e9
Strahinja Acimovic <37883394+MycroSparks@users.noreply.github.com>
2023-12-22 17:51:08 +0100

    redux implemented + base game (#4)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Provider } from "react-redux";
 import store from "./core/store/store";
@@ -10,10 +8,6 @@ function App() {
     <div className="App">
       <Provider store={store}>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
           <Main />
         </header>
       </Provider>
